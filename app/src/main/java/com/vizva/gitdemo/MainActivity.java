06cf8d3eefb4bf4e5413105bad76d2c383a499f5
GVR <ganji.venkataramana@gmail.com>
2018-02-08 22:12:05 +0530

    Second Update

---
+++ app/src/main/java/com/vizva/gitdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vizva.gitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG ="My Git Demo";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
