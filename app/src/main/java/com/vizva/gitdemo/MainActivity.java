51a0b57e73f2410d4a609af761aba80d226db0f5
GVR <ganji.venkataramana@gmail.com>
2018-02-08 22:34:00 +0530

    latest code

--- app/src/main/java/com/vizva/gitdemo/MainActivity.java
+++ app/src/main/java/com/vizva/gitdemo/MainActivity.java
@@ -10,8 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-      @Override
-    protected void onStart() {
-        super.onStart();
+
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 }
